8cddec32d4b43c52f26f79e07a0f110eafd24cf8
e1bandito <aleksandr.shitov@mail.ru>
2017-12-18 06:43:39 +0300

    Небольшие правки

--- js/backend.js
+++ js/backend.js
@@ -12,7 +12,7 @@
       if (xhr.status === 200) {
         onSuccess(xhr.response);
       } else {
-        onError(xhr.response);
+        onError('Код ошибки: ' + xhr.status);
       }
     });
 
